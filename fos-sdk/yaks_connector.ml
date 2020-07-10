4ebb321273bcb906b99bb6e7feeaf082ebb2675d
Gabriele Baldoni <gabrik@users.noreply.github.com>
2020-07-10 08:32:55 +0200

    Detailed information on vitual network (#14)
    * Added call to plugin functions for bridge/vxlan interfaces
    
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>
    
    * fix in new added functions
    
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>
    
    * updated network information fields
    
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>
    
    * removed added fields
    
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>
    
    * updating version number to 0.2.1
    
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- fos-sdk/yaks_connector.ml
+++ fos-sdk/yaks_connector.ml
@@ -2516,6 +2516,43 @@ module MakeLAD(P: sig val prefix: string end) = struct
     let p = get_node_network_floating_ip_info_path nodeid pluginid floatingid in
     Yaks.Workspace.remove p connector.ws
 
+
+  let add_node_bridge nodeid pluginid brname connector =
+    let%lwt res = exec_plugin_eval nodeid pluginid "create_bridge_interface" [("brname", brname)] connector in
+    match res with
+    | Some x -> Lwt.return x
+    | None -> Lwt.fail @@ FException (`InternalError (`Msg ("Empty value for remove_node_vxlan") ))
+
+  let remove_node_bridge nodeid pluginid brname connector =
+    let%lwt res = exec_plugin_eval nodeid pluginid "delete_bridge_interface" [("brname", brname)] connector in
+    match res with
+    | Some x -> Lwt.return x
+    | None -> Lwt.fail @@ FException (`InternalError (`Msg ("Empty value for remove_node_vxlan") ))
+
+  let attach_to_node_bridge nodeid pluginid intfname brname connector =
+    let%lwt res = exec_plugin_eval nodeid pluginid "attach_interface_to_bridge" [("intf_name", intfname);("br_name", brname)] connector in
+    match res with
+    | Some x -> Lwt.return x
+    | None -> Lwt.fail @@ FException (`InternalError (`Msg ("Empty value for remove_node_vxlan") ))
+
+  let detach_from_node_network nodeid pluginid intfname connector =
+    let%lwt res = exec_plugin_eval nodeid pluginid "detach_interface_from_bridge" [("intf_name", intfname)] connector in
+    match res with
+    | Some x -> Lwt.return x
+    | None -> Lwt.fail @@ FException (`InternalError (`Msg ("Empty value for remove_node_vxlan") ))
+
+  let add_node_vxlan nodeid pluginid vni mcast port master connector =
+    let%lwt res = exec_plugin_eval nodeid pluginid "create_vxlan_interface" [("vni", vni);("mcast_group", mcast);("port", port);("master_intf", master)] connector in
+    match res with
+    | Some x -> Lwt.return x
+    | None -> Lwt.fail @@ FException (`InternalError (`Msg ("Empty value for remove_node_vxlan") ))
+
+  let remove_node_vxlan nodeid pluginid intfname connector =
+    let%lwt res = exec_plugin_eval nodeid pluginid "delete_vxlan_interface" [("intf_name", intfname)] connector in
+    match res with
+    | Some x -> Lwt.return x
+    | None -> Lwt.fail @@ FException (`InternalError (`Msg ("Empty value for remove_node_vxlan") ))
+
 end
 
 module MakeCLAD(P: sig val prefix: string end) = struct
